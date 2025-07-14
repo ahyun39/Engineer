a8269d453e3c8e387d2f06a3c9d8b0ca64e3002b
ahyun39 <qkzy8191@gmail.com>
2025-07-14 22:27:25 +0900

    file org

--- Project/project_Stamp_App/day6_stamp/day6_stamp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('stamps.urls')),  # stamps 앱 URL 연결
-]
--- Project/project_Stamp_App/day6_stamp/stamps/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Stamp
-
-@admin.register(Stamp)
-class StampAdmin(admin.ModelAdmin):
-    list_display = ("artist_name", "concert_name", "date", "created_at")
-    search_fields = ("artist_name", "concert_name")
